179da3d7dabadc80d625545690fa30c202e3a741
thiagolucenasobrinho <thiago@teraidc.com.br>
2016-06-02 17:59:44 -0300

    Type commit message here…

--- teste/relatorio_teste.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once("lib/adm.php");
-sessao();
-logsys('Priplan - Receituario controle especial');
-$tpl=new Template(DIR."tpl/relatorio_teste.html");
-$tpl->id=$_GET['id'];
-$tpl->show();
-?>
